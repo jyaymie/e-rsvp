d2da3adfe8f2e6cde74a517b558dffaa9bd5db1d
jyaymie <104411072+jyaymie@users.noreply.github.com>
2023-01-30 00:41:56 -0800

    Create REST API

--- server/db.js
+++ server/db.js
@@ -4,21 +4,25 @@ const sqlite3 = require('sqlite3');
  * OPEN_READWRITE: The database is opened for reading and writing where the
  * database must already exist, otherwise an error is returned.
  */
-let db = new sqlite3.Database('./e-rsvp.db', sqlite3.OPEN_READWRITE, (err) => {
-	if (err && err.code == 'SQLITE_CANTOPEN') {
-		return createDatabase();
-	} else if (err) {
-		console.log('Error: ' + err);
-		exit(1);
+const db = new sqlite3.Database(
+	'./e-rsvp.db',
+	sqlite3.OPEN_READWRITE,
+	(err) => {
+		if (err && err.code === 'SQLITE_CANTOPEN') {
+			return createDatabase();
+		} else if (err) {
+			console.log('Error opening database ' + err);
+			exit(1);
+		}
+		// If the database exists, the runQueries() is executed.
+		// runQueries(db);
 	}
-	// If the database exists, the runQueries() is executed.
-	// runQueries(db);
-});
+);
 
 function createDatabase() {
-	let newdb = new sqlite3.Database('e-rsvp.db', (err) => {
+	const newdb = new sqlite3.Database('e-rsvp.db', (err) => {
 		if (err) {
-			console.log('Error: ' + err);
+			console.log('Error creating database: ' + err);
 			exit(1);
 		}
 		createTables(newdb);
@@ -30,14 +34,14 @@ function createTables(newdb) {
 		`CREATE TABLE guest (
       guest_id INTEGER PRIMARY KEY NOT NULL,
       name TEXT NOT NULL,
-      attending INTEGER NOT NULL,
-      needHotel INTEGER,
+      attending INTEGER,
+      need_hotel INTEGER,
       entree TEXT,
       food_exceptions TEXT,
       song_request TEXT
     );
 
-    INSERT INTO guest (guest_id, name, attending, needHotel, entree, food_exceptions, song_request)
+    INSERT INTO guest (guest_id, name, attending, need_hotel, entree, food_exceptions, song_request)
       VALUES (1, 'Michael Scott', 1, 1, 'Beef', '', '"Seasons of Love" by Rent'),
           (2, 'Dwight Schrute', 1, 0, 'Chicken', '', '"Wild Side" by Mötley Crüe'),
           (3, 'Angela Martin', 1, 0, 'Vegetarian', 'Developed an allergy to beets', '"The Little Drummer Boy"'),
@@ -49,17 +53,17 @@ function createTables(newdb) {
           (9, 'Ryan Howard', 1, 0, 'Beef', 'Only organic food please', ''),
           (10, 'Kevin Malone', 1, 1, 'Chicken', '', '"You Oughta Know" by Alanis Morissette');
     
-    CREATE TABLE guest_plus_one (
+    CREATE TABLE plus_one (
       guest_id INTEGER NOT NULL,
-      plus_one INTEGER NOT NULL,
-      PRIMARY KEY(guest_id, plus_one),
+      plus_one_id INTEGER NOT NULL,
+      PRIMARY KEY(guest_id, plus_one_id),
       FOREIGN KEY(guest_id) REFERENCES guest(guest_id),
-      FOREIGN KEY(plus_one) REFERENCES guest(guest_id)
+      FOREIGN KEY(plus_one_id) REFERENCES guest(guest_id)
     );
     `,
 		(err) => {
 			if (err) {
-				console.log('Error: ' + err);
+				console.log('Error creating table: ' + err);
 			}
 			runQueries(newdb);
 		}
@@ -67,7 +71,7 @@ function createTables(newdb) {
 }
 
 function runQueries(db) {
-	db.all(`SELECT * FROM guest`, (err, rows) => {
+	db.all('SELECT * FROM guest', (err, rows) => {
 		rows.forEach((row) => {
 			console.log(row);
 		});
--- server/index.js
+++ server/index.js
@@ -1,21 +1,116 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3001;
-const db = require('./db')
+const PORT = 3001;
+const db = require('./db');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res, next) => {
+// READ: Get all guests
+app.get('/guests', (req, res) => {
 	db.all('SELECT * FROM guest', [], (err, rows) => {
 		if (err) {
-			res.status(400).json({ Error: err.message });
+			res.status(400).json({ 'Error getting all guests': err.message });
 			return;
 		}
 		res.status(200).json({ rows });
 	});
 });
 
-app.listen(port, () => console.log(`✅ e-RSVP listening on port ${port}`));
+// READ: Get guest by id
+app.get('/guests/:id', (req, res) => {
+	db.get(
+		'SELECT * FROM guest WHERE guest_id = ?',
+		[req.params.id],
+		(err, rows) => {
+			if (err) {
+				res.status(400).json({ 'Error getting guest': err.message });
+				return;
+			}
+			res.status(200).json({ rows });
+		}
+	);
+});
+
+// CREATE: Add guest
+app.post('/guests', (req, res) => {
+	const {
+		guest_id,
+		name,
+		attending,
+		need_hotel,
+		entree,
+		food_exceptions,
+		song_request,
+	} = req.body;
+	db.run(
+		'INSERT INTO guest (guest_id, name, attending, need_hotel, entree, food_exceptions, song_request) VALUES (?, ?, ?, ?, ?, ?, ?)',
+		[
+			guest_id,
+			name,
+			attending,
+			need_hotel,
+			entree,
+			food_exceptions,
+			song_request,
+		],
+		(err, rows) => {
+			if (err) {
+				res.status(400).json({ 'Error adding guest': err.message });
+				return;
+			}
+			res.status(201).json({ rows });
+		}
+	);
+});
+
+// UPDATE: Update guest by id
+app.put('/guests/:id', (req, res, next) => {
+	const {
+		guest_id,
+		name,
+		attending,
+		need_hotel,
+		entree,
+		food_exceptions,
+		song_request,
+	} = req.body;
+	db.run(
+		'UPDATE guest SET name = ?, attending = ?, need_hotel = ?, entree = ?, food_exceptions = ?, song_request = ? WHERE guest_id = ?',
+		[
+			name,
+			attending,
+			need_hotel,
+			entree,
+			food_exceptions,
+			song_request,
+			guest_id,
+		],
+		(err, rows) => {
+			if (err) {
+				res.status(400).json({ 'Error updating guest': err.message });
+				return;
+			}
+			res.status(200).json({ rows });
+		}
+	);
+});
+
+// DELETE: Delete guest by id
+app.delete('/guests/:id', (req, res, next) => {
+	db.run(
+		'DELETE FROM guest WHERE guest_id = ?',
+		[req.params.id],
+		(err, rows) => {
+			if (err) {
+				res.status(400).json({ 'Error deleting guest': err.message });
+				return;
+			}
+			res.status(200).json({ rows });
+		}
+	);
+});
+
+app.listen(PORT, () => console.log('✅ Server is listening on port', PORT));
